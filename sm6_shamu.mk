8406a365db1f60a0651d84cf93b2681c105b1fb5
LV3 <cryptickid715@gmail.com>
2015-10-06 13:19:43 -0400

    du>sm6

--- sm6_shamu.mk
+++ sm6_shamu.mk
@@ -21,8 +21,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
-$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/sm6/config/common_full_phone.mk)
+$(call inherit-product, vendor/sm6/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := sm6_shamu
 PRODUCT_DEVICE := shamu
